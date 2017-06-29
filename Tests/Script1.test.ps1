5a92b03bd5d8c619c7c3712f4ec3b2f5c9bb8b03
Mike <github@barry.io>
2017-06-29 08:40:44 -0400

    Use ModuleName instead

--- Tests/Script1.test.ps1
+++ Tests/Script1.test.ps1
@@ -3,14 +3,12 @@ Import-Module "DemoModule" -Force
 Import-Module "DemoModuleTwo" -Force
 Import-Module "Pester" -Force
 
-InModuleScope DemoModule { # i can't nest this to override both
-    describe "this mock should work" {
-        it "this mock should work" {
-            Mock "Write-A" 
-            Mock "TestFileA" { $true }
-            Mock "Write-B" 
-            Mock "TestFileB" { $true }            
-            & "..\Script.ps1"
-        }
+describe "this mock should work" {
+    it "this mock should work" {
+        Mock "Write-A" 
+        Mock -ModuleName DemoModule "TestFileA" { $true }
+        Mock "Write-B" 
+        Mock -ModuleName DemoModuleTwo "TestFileB" { $true }            
+        & "..\Script.ps1"
     }
 }
